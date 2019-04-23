32fb6b952a589d36cc8466d6bb0660c21b7306d1
manugeo <manugeo13@gmail.com>
2019-04-23 13:45:54 +0530

    Creating redux store.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(rootReducer);
 
 render(<App />, document.getElementById('root'));
 
